ebd3a4c8466a211b8367f8320e21f41160214fca
ezsi <ezsias.bela@gmail.com>
2014-07-05 18:36:50 +0200

    fixing some bugs

--- edx-dl.py
+++ edx-dl.py
@@ -294,6 +294,9 @@ def main():
     COURSES = soup.find_all('article', 'course')
     courses = []
     for COURSE in COURSES:
+        print(COURSE.h3)
+        if COURSE.h3 == None:
+            continue
         c_name = COURSE.h3.text.strip()
         c_link = BASE_URL + COURSE.a['href']
         if c_link.endswith('info') or c_link.endswith('info/'):
@@ -361,7 +364,7 @@ def main():
         id_container = splitter.split(page)[1:]
         video_id += [link[:YOUTUBE_VIDEO_ID_LENGTH] for link in
                      id_container]
-        subsUrls += [BASE_URL + regexpSubs.search(container).group(2) + "?videoId=" + id + "&language=en"
+        subsUrls += [BASE_URL + regexpSubs.search(container).group(1) + "?videoId=" + id + "&language=en"
                      if regexpSubs.search(container) is not None else ''
                      for id, container in zip(video_id[-len(id_container):], id_container)]
         # Try to download some extra videos which is referred by iframe
